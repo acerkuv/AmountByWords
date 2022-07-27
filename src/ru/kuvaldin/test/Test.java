d68468a0498730302541b24663f956622b300897
Eugene kuvaldin <acerkuv@gmail.com>
2022-07-27 14:57:43 +0300

    First Upload

--- src/ru/kuvaldin/test/Test.java
+++ src/ru/kuvaldin/test/Test.java
@@ -6,9 +6,11 @@ public class Test {
     TestMashine testMashine = new TestMashine();
     testMashine.check("1", "один ");
     testMashine.check("101", "сто один ");
+    testMashine.check("100027", "сто тысяч двадцать семь ");
     testMashine.check("77709", "семьдесят семь тысяч семьсот девять ");
     testMashine.check("889759", "восемьсот восемьдесят девять тысяч семьсот пятьдесят девять ");
     testMashine.check("100001", "сто тысяч один ");
+    testMashine.check("127988","сто двадцать семь тысяч девятьсот восемьдесят восемь ");
 
 
 
